284708a70494aaf590171713b47ba6b3b79c9173
Alberto Di Maria <alberto.dimaria@mail.polimi.it>
2025-03-04 15:13:48 +0100

    Fixed E0304 error: Explicit cast to Slope in updateCutFilter

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -107,16 +107,9 @@ void SimpleEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
     leftChain.prepare(spec);
     rightChain.prepare(spec);
 
-	auto chainSettings = getChainSettings(apvts);
-	updatePeakFilter(chainSettings);
+	updateFilters();
 
-	// auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
-	// updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
-
-    // auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
-    // updateCutFilter(rightLowCut, cutCoefficients, chainSettings.lowCutSlope);
-
-   
+	
 
 }
 
@@ -167,20 +160,7 @@ void SimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-	auto chainSettings = getChainSettings(apvts);
-
-	updatePeakFilter(chainSettings);
-
-    auto cutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(
-        chainSettings.lowCutFreq, getSampleRate(), 2 * (chainSettings.lowCutSlope + 1));
-
-
-	// auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
-	// updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
-    
-	// auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
-	// updateCutFilter(rightLowCut, cutCoefficients, chainSettings.lowCutSlope);
-
+	updateFilters();
 
     juce::dsp::AudioBlock<float> block(buffer);
 
@@ -253,6 +233,49 @@ void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings
 void SimpleEQAudioProcessor::updateCoefficients(Coefficients& old, const Coefficients& replacements)
 {
 	*old = *replacements;
+}
+
+void SimpleEQAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings)
+{
+	auto cutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(
+		chainSettings.lowCutFreq, getSampleRate(), 2 * (chainSettings.lowCutSlope + 1));
+	
+    auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
+    auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
+
+    // updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
+    // updateCutFilter(rightLowCut, cutCoefficients, chainSettings.lowCutSlope);
+    updateCutFilter(leftLowCut, cutCoefficients, static_cast<Slope>(chainSettings.lowCutSlope));
+    updateCutFilter(rightLowCut, cutCoefficients, static_cast<Slope>(chainSettings.lowCutSlope));
+
+
+}
+
+void SimpleEQAudioProcessor::updateHighCutFilters(const ChainSettings& chainSettings)
+{
+    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(
+        chainSettings.highCutFreq, getSampleRate(), 2 * (chainSettings.lowCutSlope + 1));
+
+	auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
+	auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
+
+	// updateCutFilter(leftHighCut, highCutCoefficients, chainSettings.highCutSlope);
+	// updateCutFilter(rightHighCut, highCutCoefficients, chainSettings.highCutSlope);
+
+    updateCutFilter(leftHighCut, highCutCoefficients, static_cast<Slope>(chainSettings.lowCutSlope));
+    updateCutFilter(rightHighCut, highCutCoefficients, static_cast<Slope>(chainSettings.lowCutSlope));
+
+
+}
+
+
+void SimpleEQAudioProcessor::updateFilters()
+{
+    auto chainSettings = getChainSettings(apvts);
+
+    updateLowCutFilters(chainSettings);
+    updatePeakFilter(chainSettings);
+    updateHighCutFilters(chainSettings);
 }
 
 juce::AudioProcessorValueTreeState::ParameterLayout
